ff2709095ab9047f77c9f3a5382bb03e145e1a23
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-03-02 16:17:09 +0200

    method overloading test

---
+++ test/method_overloading.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: iceman
+ * Date: 3/2/13
+ * Time: 3:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+interface IPoliMethods
+{
+
+    public function poli($a);
+
+//    public function poli($a, $b);
+}
+
+class PoliMetods implements IPoliMethods
+{
+
+    public function poli($a)
+    {
+        echo "Metoda cu un parametru " . $a . "</br>";
+
+    }
+
+//    public function poli($a, $b)
+//    {
+//        echo "Metoda cu doi parametri " . $a . " si " . $b . "</br>";
+//    }
+}
+
+$obj = new PoliMetods();
+$obj.poli(1);
+$obj.poli(1, 2);
+?>
