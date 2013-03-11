de95115532d9c13770c31d826ba5305482defd7b
Alex Burghelea <iceman.ftg@gmail.com>
2013-03-11 13:01:49 +0200

    Game states constants

---
+++ game/GameEngine.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: Alexandru George Burghelea
+ * Date: 11.03.2013
+ * Time: 12:42
+ * For : PWeb 2013
+ */
+class GameEngine
+{
+
+}
---
+++ game/GameState.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * User: Alexandru George Burghelea
+ * Date: 11.03.2013
+ * Time: 12:52
+ * For : PWeb 2013
+ */
+
+class GameState
+{
+    const WAITING_PLAYERS = 'WAITING_PLAYERS ';
+    const PLANET_CLAIM = 'PLANET_CLAIM';
+    const SHIP_PLACING = 'SHIP_PLACING';
+    const ATTACK = 'ATTACK';
+    const GAME_END = 'GAME_END';
+}
+
+?>
