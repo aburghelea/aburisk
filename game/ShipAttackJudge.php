83c6191ec1a11cbaf7f731eb83c4eb394a0aaf7b
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-03-13 00:42:05 +0200

    Ship Attack Judge

---
+++ game/ShipAttackJudge.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * User: Alexandru George Burghelea
+ * Date: 3/12/13
+ * Time: 10:29 PM
+ * For : PWeb 2013
+ */
+class ShipAttackJudge
+{
+
+    /**
+     * @param int $attacker_ships number of attacking ships
+     * @param int $defender_ships number of defending ships
+     * @param bool $bonus_atack if the attacker is using less ships then possible
+     * @return int|array -1 if the attack is not possible, (remaining ships on attack, remaining ships on defence, conquering_ships)
+     */
+    public static function judge($attacker_ships, $defender_ships, $total_atacking)
+    {
+        $no_att_dice = self::attacking(min($attacker_ships, $total_atacking+1));
+        $no_def_dice = self::defending($defender_ships);
+        echo "INP " . $attacker_ships . " " . $defender_ships ." ".$total_atacking. "\n";
+        $dice = min($no_att_dice, $no_def_dice);
+        if ($dice <= 0)
+            return -1;
+
+        $attacking_dice = array();
+        $defending_dice = array();
+        for ($i = 0; $i < $dice; $i++) {
+            $attacking_dice[] = rand(1, 6);
+            $defending_dice[] = rand(1, 6);
+        }
+        rsort($attacking_dice);
+        rsort($defending_dice);
+
+        $attack_casualties = 0;
+        $defend_casualties = 0;
+        for ($i = 0; $i < $dice; $i++) {
+            echo "Dice ".$attacking_dice[$i]." ".$defending_dice[$i]."\n";
+            if ($attacking_dice[$i] > $defending_dice[$i])
+                $defend_casualties++;
+            else
+                $attack_casualties++;
+        }
+        echo "casualties ".$attack_casualties." ".$defend_casualties."\n";
+        $attacker_ships = max(1, ($attacker_ships - $attack_casualties));
+        $defender_ships = max(0, ($defender_ships - $defend_casualties));
+        echo 'MIN '. min($total_atacking, ($no_att_dice - $attack_casualties))."\n";
+        $conquering_ships = $defender_ships == 0 ? min($total_atacking, ($no_att_dice - $attack_casualties)) : 0;
+        if ($conquering_ships > 0) {
+            $conquering_ships = $conquering_ships < $attacker_ships ? $conquering_ships : ($attacker_ships - 1);
+            $attacker_ships -= $conquering_ships;
+        }
+
+        return array("A" => $attacker_ships, "D" => $defender_ships, "C" => $conquering_ships);
+
+    }
+
+    private static function attacking($ships)
+    {
+        if ($ships < 2)
+            return -1;
+        else if ($ships == 2)
+            return 1;
+        else if ($ships == 3)
+            return 2;
+
+        return 3;
+    }
+
+    private static function defending($ships)
+    {
+        if ($ships < 1)
+            return -1;
+        else if ($ships == 1)
+            return 1;
+
+        return 2;
+    }
+}
+
+?>
