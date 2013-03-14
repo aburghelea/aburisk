f96e3a7e3460b5904e2d3bd288e2d5e402838d2c
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-03-14 21:47:17 +0200

    create index page for scripts

--- scripts/index.php
+++ scripts/index.php
@@ -1,7 +1,15 @@
-<?php
-/**
- * User: Alexandru George Burghelea
- * Date: 3/14/13
- * Time: 9:39 PM
- * For : PWeb 2013
- */
+<div>
+    <a href="create-user.php?username=student&email=student@cs.curs.pub.ro&password=student">
+        CREATE USER student/student
+    </a>
+</div>
+<div>
+    <a href="login.php?username=iceman&password=1">LOGIN iceman/1 </a>
+</div>
+<div>
+    <a href="login.php?username=iceman&password=2">LOGIN iceman/2 </a>
+</div>
+<div>
+    <a href="login.php?username=Dasda&password=ssdasd">LOGIN Dasda/ssdasd </a>
+
+</div>
