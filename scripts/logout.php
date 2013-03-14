f4485a0b61813e772fb9274bfad5e0fa7d286c5d
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-03-14 21:53:53 +0200

    logout script

---
+++ scripts/logout.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: Alexandru George Burghelea
+ * Date: 3/14/13
+ * Time: 9:46 PM
+ * For : PWeb 2013
+ */
+require_once("../dao/User.php");
+$logoutStatus = User::logout();
+
+if  ($logoutStatus < 0 )
+    echo "NOT IMPLEMENTED YET<br/>";
+
+?>
