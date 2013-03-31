eac35b3d24a5eb24bf221a3b0a8bd1493ca1834a
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-03-31 21:24:01 +0300

    basic svg map

--- html/game_interface.php
+++ html/game_interface.php
@@ -6,11 +6,40 @@
 <div id="wrapper">
     <?php require_once "header.php" ?>
     <div id="page">
-        <div id="content-center" style="width: 960px">
-            <!----------- ADD HERE ------------>
-            GAME_SVG_HEREGAME_SVG_HEREGAME_SVG_HEREGAME_SVG_HEREGAME_SVG_HEREGAME_SVG_HEREGAME_SV
-            G_HEREGAME_SVG_HEREGAME_SVG_
-            HEREGAME_SVG_HEREGAME_SVG_HEREGAME_SVG_HEREGAME_SVG_HERE
+        <div id="content">
+<!--                        <img src="html/map.svg" width="768" height="432" alt="Nice green circle"/>-->
+            <object type="image/svg+xml" width="750" height="421" data="html/map.svg"></object>
+        </div>
+        <div id="sidebar">
+            <div id="tbox1">
+                <h2>Mauris vulputate dolor</h2>
+                <ul class="style2">
+                    <li class="first">
+                        <h3><a href="#">Maecenas luctus lectus</a></h3>
+
+                        <p><a href="#">Quisque dictum integer nisl risus, sagittis convallis, rutrum id, congue, and
+                                nibh.</a></p>
+                    </li>
+                    <li>
+                        <h3><a href="#">Integer gravida nibh</a></h3>
+
+                        <p><a href="#">Quisque dictum integer nisl risus, sagittis convallis, rutrum id, congue, and
+                                nibh.</a></p>
+                    </li>
+                    <li>
+                        <h3><a href="#">Fusce ultrices fringilla</a></h3>
+
+                        <p><a href="#">Quisque dictum integer nisl risus, sagittis convallis, rutrum id, congue, and
+                                nibh.</a></p>
+                    </li>
+                    <li>
+                        <h3><a href="#">Nulla luctus eleifend</a></h3>
+
+                        <p><a href="#">Quisque dictum integer nisl risus, sagittis convallis, rutrum id, congue, and
+                                nibh.</a></p>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 
